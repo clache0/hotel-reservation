1c2a3f12706b32c5cd626dec7885b38df5dd72b2
Clayton Cheng <clay578@gmail.com>
2023-05-11 15:42:12 -0700

    re-add data with file name change

---
+++ src/assets/languageData.js
@@ -0,0 +1,17 @@
+export default [
+    "English",
+    "Español",
+    "Français",
+    "Deutsch",
+    "Italiano",
+    "Português",
+    "普通话",
+    "日本語",
+    "한국어",
+    "العربية",
+    "Русский",
+    "Nederlands",
+    "Svenska",
+    "Norsk",
+    "Dansk"
+]
